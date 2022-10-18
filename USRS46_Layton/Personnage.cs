2d205e0c81e829440495b3dd16493132a2abb09e
Enki BACHELIER <enki.bachelier@free.fr>
2022-10-18 13:59:27 +0200

    Create Class and Complete Personnage

---
+++ USRS46_Layton/Personnage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USRS46_Layton
+{
+    public abstract class Personnage
+    {
+        private string nom;
+        private int age;
+        private string role;
+
+        public Personnage(string nom, int age, string role)
+        {
+            this.nom = nom;
+            this.age = age;
+            this.role = role;
+        }
+
+        public virtual string SePresenter()
+        {
+            return ("Bonjour ! Je m'appelle " + this.nom + " (" + this.age + " ans) !\n");
+        }
+    }
+}
---
+++ USRS46_Layton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USRS46_Layton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
